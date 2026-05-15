d1db40e5c6e688b8dc15d410f83d3ec7dd51b451
Adriano Coutinho <adriano.coutinho.ti@gmail.com>
2026-05-15 13:51:08 -0300

    Js. foi adicionado o event listener no button

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues() {
+    console.log ("A funcão foi.")
+}
+
+convertButton.addEventListener("click", convertValues)
