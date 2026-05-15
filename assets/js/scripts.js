26528cb728284620ec6aaf5ff8d1bae7e58d2957
Adriano Coutinho <adriano.coutinho.ti@gmail.com>
2026-05-15 15:57:22 -0300

    Finalizado o modulo do projeto

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 
-const convertButton = document.querySelector(".convert-button") 
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
@@ -7,19 +8,46 @@ function convertValues() {
     const currencyValueConverted = document.querySelector(".currency-value")
 
     const dolarToday = 5.2
-
-    const convertedValue = inputCurrencyValue / dolarToday
+    const euroToday = 6.2
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(inputCurrencyValue)
 
-    console.log(convertedValue)
+    if (currencySelect.value == "USD") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == "EUR") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
 }
 
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+
+    if (currencySelect.value == "USD") {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImage.src = "./assets/img/estados-unidos.png"
+    }
+
+    if (currencySelect.value == "EUR") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/img/euro.png"
+    }
+
+    convertValues()
+    
+}
+
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
